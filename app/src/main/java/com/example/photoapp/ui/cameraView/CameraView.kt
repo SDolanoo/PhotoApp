256daac767118551ffd1c41365c76495899b8b81
Dolan <sdolazinski@gmail.com>
2025-07-23 23:13:17 +0200

    CameraView.kt zdjecie wyświetla się na cały ekran i nie jest powiększone

--- app/src/main/java/com/example/photoapp/ui/cameraView/CameraView.kt
+++ app/src/main/java/com/example/photoapp/ui/cameraView/CameraView.kt
@@ -1,5 +1,6 @@
 package com.example.photoapp.ui.cameraView
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
@@ -10,6 +11,7 @@ import android.provider.MediaStore
 import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.AspectRatio
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
 import androidx.camera.core.ImageCaptureException
@@ -17,14 +19,18 @@ import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.Icons
@@ -44,6 +50,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
@@ -94,6 +101,7 @@ fun CameraView(
     }
 }
 
+@SuppressLint("ConfigurationScreenWidthHeight")
 @Composable
 fun MakePhoto(
     outputDirectory: File,
@@ -108,9 +116,19 @@ fun MakePhoto(
     val lifecycleOwner = androidx.lifecycle.compose.LocalLifecycleOwner.current
     Log.i("Dolan", "lifecycleOwner = $lifecycleOwner")
 
-    val preview = Preview.Builder().build()
-    val previewView = remember { PreviewView(context) }
-    val imageCapture: ImageCapture = remember { ImageCapture.Builder().build() }
+    val preview = Preview.Builder()
+        .setTargetAspectRatio(AspectRatio.RATIO_16_9)
+        .build()
+    val previewView = remember {
+        PreviewView(context).apply {
+            scaleType = PreviewView.ScaleType.FIT_CENTER
+        }
+    }
+
+    val imageCapture = ImageCapture.Builder()
+        .setTargetAspectRatio(AspectRatio.RATIO_16_9)
+        .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+        .build()
     val cameraSelector = CameraSelector.Builder()
         .requireLensFacing(lensFacing)
         .build()
@@ -132,7 +150,13 @@ fun MakePhoto(
         preview.setSurfaceProvider(previewView.surfaceProvider)
     }
 
-    Box(modifier = Modifier.fillMaxSize()) {
+    val screenWidth = LocalConfiguration.current.screenWidthDp.dp
+    val previewHeight = screenWidth * (16f / 9f) // <- 9:16 proporcje
+
+    Box(modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight()
+        .background(Color.Black)) {
         AndroidView({ previewView }, modifier = Modifier.fillMaxSize())
 
         IconButton(
